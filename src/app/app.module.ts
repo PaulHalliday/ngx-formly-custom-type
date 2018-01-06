d016aa7055a69fe97a1b2fbe5b990ddcdc1ce48c
Paul Halliday <hello@paulhalliday.io>
2018-01-06 09:20:34 +0000

    Add validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 
@@ -12,28 +12,44 @@ import { RepeatTypeComponent } from './repeat-section.type';
 @NgModule({
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,    
+    BrowserAnimationsModule,
     CommonModule,
     ReactiveFormsModule,
     FormlyBootstrapModule,
     FormlyModule.forRoot({
       types: [
         { name: 'repeat', component: RepeatTypeComponent },
+        {
+          name: 'name',
+          extends: 'input',
+          defaultOptions: {
+            validators: {
+              name: (control: FormControl, field: any) => {
+                const value: string = control.value;
+                let valid = false;
+                if (value && value.split(' ')[1]) {
+                  console.log(value.split(' ')[1]);
+                  valid = true;
+                }
+                return valid;
+              },
+            },
+            validation: {
+              messages: {
+                name: (err, field: any) =>
+                  'You need to provide a first and last name!',
+              },
+            },
+          },
+        },
       ],
     }),
   ],
-  declarations: [
-    AppComponent,
-    RepeatTypeComponent,
-  ],
+  declarations: [AppComponent, RepeatTypeComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
-
-
-
+export class AppModule {}
 
 // import { BrowserModule } from '@angular/platform-browser';
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -47,10 +63,7 @@ export class AppModule { }
 
 // import { AppComponent } from './app.component';
 
-
 // import { FormlyFieldCustomInput } from './custom-input.component';
-
-
 
 // @NgModule({
 //   imports: [
@@ -77,17 +90,6 @@ export class AppModule { }
 //   bootstrap: [AppComponent]
 // })
 // export class AppModule { }
-
-
-
-
-
-
-
-
-
-
-
 
 // import { BrowserModule } from '@angular/platform-browser';
 // import { NgModule } from '@angular/core';
